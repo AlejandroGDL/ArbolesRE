c144ee4bd7da888570dd1b9390ca5f6d4be8cabf
Alejandro Guevara de Luna <alejandrogdlcecyt@gmail.com>
2023-11-28 22:59:45 -0600

    Login WIP 80
    Login WIP 80% error cualquier password

--- src/hook/UseAuth.js
+++ src/hook/UseAuth.js
@@ -1,22 +1,52 @@
 import clienteAxios from "../config/axios";
 import { useNavigate } from 'react-router-dom'
+import useSWR from 'swr'
+import { useEffect } from "react";
 
 export const useAuth = ({middleware,url}) => {
+
+    const token = localStorage.getItem('AUTH_TOKEN')
+
+    const {data: user,error, mutate} = useSWR('/api/user', () => 
+        clienteAxios.get('/api/user', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.data)
+        .catch(error => {
+            throw Error(error?.response?.data?.errors)
+        })
+    )
 
     var Navegacion = useNavigate();
     
     const login = async(datos) => {
         try {
             const {data} = await clienteAxios.post('/api/Login', datos)
-            localStorage.setItem('AUTH TOKEN', data.token)
-            Navegacion('/Menu')
+            localStorage.setItem('AUTH_TOKEN', data.token)
+            
+            await mutate()
         } catch (error) {
             console.log(error.response.data.errors)
         }
     }
 
+    console.log(user)
+    console.log(error)
+    useEffect(() => {
+        if(middleware === 'guest' && url && user) {
+            Navegacion(url)
+        }
+        if(middleware === 'auth' && error) {
+            Navegacion('/')
+        }
+    },[user,error])
+
     return {
-        login
+        login,
+        user,
+        error,
     }
 }
 
--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,10 @@
+import useAuth from "../hook/UseAuth"
 import Menu from "../views/Menu"
 
 export default function AuthLayout() {
+    const {user,error} = useAuth({middleware: 'auth'});
+
+    console.log(user,error);
     return(
         <Menu/>
     )
